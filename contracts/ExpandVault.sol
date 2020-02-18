9009f272b1747aca584ceb07259d2fb03c74f2ce
Kieran O'Reilly <kieran@gems.org>
2020-02-18 17:51:09 -0500

    Remove withdrawal limit

--- contracts/ExpandVault.sol
+++ contracts/ExpandVault.sol
@@ -19,9 +19,6 @@ contract ExpandVault is Ownable, Pausable {
         mapping (uint => Withdrawal) withdrawals;
     }
 
-    uint constant internal MAX_WITHDRAW_PERIOD = 1 days;
-    uint constant internal MAX_WITHDRAW_AMOUNT = 1000000*10**18;
-
     ERC20 internal xpn;
 
     mapping (address => Address) internal log;
@@ -67,36 +64,8 @@ contract ExpandVault is Ownable, Pausable {
         nonzeroValue(_value)
         returns (bool)
     {
-        logWithdrawal(_to, _value);
         xpn.transfer(_to, _value);
        	emit Withdrew(_to, _value);
         return true;
-    }
-
-   function logWithdrawal(address _to, uint _value)
-        internal
-    {
-        uint sum;
-        bool inserted;
-        Withdrawal memory withdrawal = Withdrawal(now, _value);
-        for (uint i = 0; i < log[_to].length; i++) {
-            if (log[_to].withdrawals[i].t <= now-MAX_WITHDRAW_PERIOD) {
-                if (!inserted) {
-                    log[_to].withdrawals[i] = withdrawal;
-                    sum += withdrawal.value;
-                    inserted = true;
-                } else {
-                    log[_to].withdrawals[i] = Withdrawal(0, 0);
-                }
-            } else {
-                sum += log[_to].withdrawals[i].value;
-            }
-        }
-        if (!inserted) {
-            log[_to].withdrawals[log[_to].length] = withdrawal;
-            log[_to].length++;
-            sum += withdrawal.value;
-        }
-        require(sum <= MAX_WITHDRAW_AMOUNT);
     }
 }
